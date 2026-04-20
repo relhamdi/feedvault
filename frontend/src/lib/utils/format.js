6acd312e0341363a290f47baab526a4cc26ac23a
relhamdi <elhamdi.rayan@gmail.com>
2026-04-20 17:09:40 +0200

    Add adjustments to BBCode formatting

--- frontend/src/lib/utils/format.js
+++ frontend/src/lib/utils/format.js
@@ -17,12 +17,16 @@ export function formatDate(dateStr) {
 export function parseBBCode(str) {
     if (!str) return '';
     return str
+        .replace(/<br\s*\/?>/gi, '\n') // Normalize existing <br> to \n first
         .replace(/\[b\](.*?)\[\/b\]/gis, '<strong>$1</strong>')
         .replace(/\[i\](.*?)\[\/i\]/gis, '<em>$1</em>')
         .replace(/\[u\](.*?)\[\/u\]/gis, '<u>$1</u>')
         .replace(/\[s\](.*?)\[\/s\]/gis, '<s>$1</s>')
         .replace(/\[color=(.*?)\](.*?)\[\/color\]/gis, '<span style="color:$1">$2</span>')
-        .replace(/\[size=(\d+)\](.*?)\[\/size\]/gis, '<span style="font-size:$1px">$2</span>')
+        .replace(
+            /\[size=(\d+)\](.*?)\[\/size\]/gis,
+            (_, size, content) => `<span style="font-size:${size * 4}px">${content}</span>`
+        )
         .replace(
             /\[url=(.*?)\](.*?)\[\/url\]/gis,
             '<a href="$1" target="_blank" rel="noopener noreferrer">$2</a>'
@@ -31,5 +35,5 @@ export function parseBBCode(str) {
             /\[url\](.*?)\[\/url\]/gis,
             '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>'
         )
-        .replace(/\n/g, '<br>');
+        .replace(/\r?\n/g, '<br>'); // Convert all \n to <br> at the end
 }
