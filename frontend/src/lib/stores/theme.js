d0e73ae2e6fc1e62b2bb4517070b48b3811074da
relhamdi <elhamdi.rayan@gmail.com>
2026-04-20 14:20:42 +0200

    Lint js files

--- frontend/src/lib/stores/theme.js
+++ frontend/src/lib/stores/theme.js
@@ -3,7 +3,7 @@ import { writable } from 'svelte/store';
 const stored = localStorage.getItem('theme') || 'light';
 export const theme = writable(stored);
 
-theme.subscribe(value => {
+theme.subscribe((value) => {
     localStorage.setItem('theme', value);
     document.documentElement.setAttribute('data-theme', value);
 });
