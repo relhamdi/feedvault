49a3ba0e91edd5aefa51be0b2ed4b0059ef4fc20
relhamdi <elhamdi.rayan@gmail.com>
2026-05-17 17:18:48 +0200

    Add clearItems route support

--- frontend/src/lib/api/feeds.js
+++ frontend/src/lib/api/feeds.js
@@ -19,4 +19,5 @@ export const feedsApi = {
     create: (data) => api.post('/feeds/', data),
     update: (id, data) => api.patch(`/feeds/${id}`, data),
     delete: (id) => api.delete(`/feeds/${id}`),
+    clearItems: (id) => api.delete(`/feeds/${id}/items`),
 };
